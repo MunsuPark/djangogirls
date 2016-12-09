2222cd9822a200c9530e38ebc8177d219c6b38c9
munsu <mspark@8percent.kr>
2016-12-10 05:24:11 +0900

    FIX: 오타수정.

--- mysite/settings.py
+++ mysite/settings.py
@@ -47,7 +47,7 @@ MIDDLEWARE_CLASSES = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleare',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
